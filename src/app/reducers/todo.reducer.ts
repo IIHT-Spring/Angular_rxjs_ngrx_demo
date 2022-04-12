992e26d6b37a1a30e80b75c2976bb15bce6991ba
ngactive99 <ngactive99@gmail.com>
2022-04-12 10:11:07 +0530

    store and reducer setup

--- src/app/reducers/todo.reducer.ts
+++ src/app/reducers/todo.reducer.ts
@@ -2,8 +2,14 @@ import { TodoActionType } from "../shared/enum/todo-action-types.enum";
 import { ActionParent } from "../actions/todo.actions";
 import { Todo } from "../models/Todo";
 
-const initialState: Todo[] = [
+export const initialState: Todo[] = [
     {title: "Todo 1"},
     {title: "Todo 2"},
     {title: "Todo 3"}
-];
+]; 
+
+export function TodoReducer(state = initialState, action: ActionParent){
+    switch(action.type){
+        default: return state;
+    }
+}
