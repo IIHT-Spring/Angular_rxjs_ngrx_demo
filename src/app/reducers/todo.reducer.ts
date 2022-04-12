c4f06019260b9370edaa64256e89685949fc33a1
ngactive99 <ngactive99@gmail.com>
2022-04-12 11:38:53 +0530

    add todo funcationality completed

--- src/app/reducers/todo.reducer.ts
+++ src/app/reducers/todo.reducer.ts
@@ -3,9 +3,8 @@ import { ActionParent } from "../actions/todo.actions";
 import { Todo } from "../models/Todo";
 
 export const initialState: Todo[] = [
-    {title: "Todo 1"},
-    {title: "Todo 2"},
-    {title: "Todo 3"}
+    {title: ""},
+   
 ]; 
 
 export function TodoReducer(state = initialState, action: ActionParent){
